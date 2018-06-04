627077c3984aa2d64c5c5dc149d603783e27e069
Sebastian <33915497+SebasttianVelez@users.noreply.github.com>
2018-06-04 16:23:07 -0500

    Add files via upload

---
+++ Objects/Plataforma.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+from functions.cut import CortarImagen
+
+img1 = pygame.image.load('Images/Plataformas/Plataforma11.png')
+
+class Plataforma(pygame.sprite.Sprite):
+	def __init__(self, px, py):
+		pygame.sprite.Sprite.__init__(self)
+		self.cut = CortarImagen(img1, 0, 0, 1, 1)
+		self.image = self.cut
+		self.rect = self.image.get_rect()
+		self.rect.x= px
+		self.rect.y = py
+		self.vel_x = 0
+		self.vel_y = 0
+	def update(self):
+		self.image = self.cut
+		self.rect.x += self.vel_x
+		self.rect.y += self.vel_y
